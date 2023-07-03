67abf4f6f1e91aced31dc3ac3e5f3cc552334847
asb21 <asb2@cesar.org.br>
2023-07-03 10:30:14 -0300

    feature/include entropy calculus

--- BinaryLZW/BinaryLZWCompression.java
+++ BinaryLZW/BinaryLZWCompression.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 public class BinaryLZWCompression {
     private static final int MAX_TABLE_SIZE = 4096*8;
@@ -47,6 +50,30 @@ public class BinaryLZWCompression {
         }
     }
 
+    public static double entropy(byte[] input) {
+        if (input.length == 0) {
+            return 0.0;
+        }
+
+        int[] charCounts = new int[256];
+
+        for (byte b : input) {
+            charCounts[b & 0xFF]++;
+        }
+
+        double entropy = 0.0;
+        for (int i = 0; i < 128; ++i) {
+
+            if (charCounts[i] == 0.0) {
+                continue;
+            }
+            double freq = (double) charCounts[i] / input.length;
+            entropy -= freq * (Math.log(freq) / Math.log(2));
+        }
+
+        return entropy;
+    }
+
     public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Usage: java BinaryLZWCompression <inputFile> <compressedFile>");
@@ -62,6 +89,31 @@ public class BinaryLZWCompression {
         compress(inputFile, compressedFile);
 
         System.out.println("Compression completed successfully.");
+        
+        System.out.println( "**********************");
+        System.out.println( "Tamanho arquivo de entrada: " + inputFile.length());
+        System.out.println( "Tamanho arquivo de saida: " + compressedFile.length());
+        
+        
+        try {
+        double inputEntropy = entropy(Files.readAllBytes(inputFile.toPath()));
+        double outputEntropy = entropy(Files.readAllBytes(compressedFile.toPath()));
+        
+        System.out.println( "Entropia de entrada: " + inputEntropy);
+        System.out.println( "Entropia de saida: " + outputEntropy);
+
+        double rateCompression = inputEntropy / outputEntropy;
+        System.out.println( "Taxa de compressao em relacao a entropia: " + rateCompression);
+        // Continue processing with the calculated entropies
+        } catch (IOException e) {
+        // Handle the exception
+        e.printStackTrace(); // Print the stack trace (optional)
+        // Additional error handling logic if needed
+        }
+
+        
+
+
     }
 
     private static Map<List<Byte>, Short> createDictionary() {
